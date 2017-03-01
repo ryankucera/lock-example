50570b521f5414e86d056f241e5e0a82cda0b121
Phil True <phil@maladon.com>
2017-03-01 15:32:42 -0600

    now logs state and command to Tsdb

--- eventhandlers/device_datapoint.lua
+++ eventhandlers/device_datapoint.lua
@@ -1,49 +1,53 @@
 --#EVENT device datapoint
 --[[
-  data
-  The data from the device
+data
+The data from the device
 
-  data.api enum string (write|record)
-  Provider API
+data.api enum string (write|record)
+Provider API
 
-  data.rid string
-  Unique device resource id
+data.rid string
+Unique device resource id
 
-  data.seq integer
-  The message sequence number for specific resource id
+data.seq integer
+The message sequence number for specific resource id
 
-  data.alias string
-  Device resource alias
+data.alias string
+Device resource alias
 
-  data.value table{1 = "live"|timestamp, 2 = value}
-  Data transmitted by the device
+data.value table{1 = "live"|timestamp, 2 = value}
+Data transmitted by the device
 
-  data.vendor string
-  Device vendor identifier
+data.vendor string
+Device vendor identifier
 
-  data.device_sn string
-  Device Serial number
+data.device_sn string
+Device Serial number
 
-  data.source_ip string
-  The device source ip
+data.source_ip string
+The device source ip
 
-  data.timestamp integer
-  Event time
+data.timestamp integer
+Event time
 --]]
 
 -- update the state for this lock
 util.setStates('lockID', data.device_sn, {
-  data.alias, data.value[2], 
-  'last_timestamp', data.timestamp
+    data.alias, data.value[2],
+    'last_timestamp', data.timestamp
 })
+
+if data.alias == 'lock-state' or data.alias == 'lock-command' then
+    util.saveHistory(data.device_sn, data.alias, data.value[2])
+end
 
 -- notify UI of an update
 -- get websocket ids
 local sockets = Websocket.list()
 for k, socket in pairs(sockets) do
-	-- send update on each socket
-	Websocket.send({
-		socket_id = socket,
-		message = to_json({lockID = data.device_sn, [data.alias] = data.value[2]})
-	})
+    -- send update on each socket
+    Websocket.send({
+        socket_id = socket,
+        message = to_json({ lockID = data.device_sn, [data.alias] = data.value[2] })
+    })
 end
--- modules/util.lua
+++ modules/util.lua
@@ -2,101 +2,118 @@ util = {}
 
 -- get current logged in user from webservice request
 -- returns user table, or nil if no user token is contained
--- in cookie or token headers. Returned user table looks 
+-- in cookie or token headers. Returned user table looks
 -- like this:
 -- {
---   "creation_date":1.477266383e+09,
---   "email":"frank@exosite.com",
---   "id":2,
---   "name":"frank",
--- 	 "status":1,
---   "token":"eyJ0e..."
+-- "creation_date":1.477266383e+09,
+-- "email":"frank@exosite.com",
+-- "id":2,
+-- "name":"frank",
+-- "status":1,
+-- "token":"eyJ0e..."
 -- }
-util.currentUser = function (request)
-  return util.currentUserFromHeaders(request.headers)
+util.currentUser = function(request)
+    return util.currentUserFromHeaders(request.headers)
 end
 
 -- determine the current user from the session information
 -- stored in webservice or websocket request headers.
 -- returns user table or nil if no user is contained
 -- in headers
-util.currentUserFromHeaders = function (headers)
-	local token = nil
-	if type(headers.token) == "string" then
-		-- the caller passed a token header. Use that.
-		token = headers.token
-	else
-		-- check for the token in the session in a cookie
-		if type(headers.cookie) == "string" then
-			local _, _, token = string.find(headers.cookie, "sid=([^;]+)")
-			if type(token) ~= "string" then
-				return nil
-			end
-		else 
-			return nil
-		end
-  end
-  local user = User.getCurrentUser({token = token})
-  if user ~= nil and user.id ~= nil then
-    user.token = token
-    return user
-  end
-  return nil
+util.currentUserFromHeaders = function(headers)
+    local token = nil
+    if type(headers.token) == "string" then
+        -- the caller passed a token header. Use that.
+        token = headers.token
+    else
+        -- check for the token in the session in a cookie
+        if type(headers.cookie) == "string" then
+            local _, _, token = string.find(headers.cookie, "sid=([^;]+)")
+            if type(token) ~= "string" then
+                return nil
+            end
+        else
+            return nil
+        end
+    end
+    local user = User.getCurrentUser({ token = token })
+    if user ~= nil and user.id ~= nil then
+        user.token = token
+        return user
+    end
+    return nil
 end
 
 -- get all the states for this device from the hash
 -- parm_name is 'lockID' or 'dwellingID', parm_value is the ID
 util.getStates = function(parm_name, parm_value)
-	-- http://redis.io/commands/hgetall
-	return Keystore.command({
-		command = 'hgetall',
-		key = 'state:' .. parm_name .. ':' .. parm_value
-	}).value
+    -- http://redis.io/commands/hgetall
+    return Keystore.command({
+        command = 'hgetall',
+        key = 'state:' .. parm_name .. ':' .. parm_value
+    }).value
 end
 
 -- set one or more states for a given lock or dwelling
 -- parm_name is 'lockID' or 'dwellingID', parm_value is the ID
 -- states is a table like {field1, value1, field2, value2, ...}
 util.setStates = function(parm_name, parm_value, states)
-	-- http://redis.io/commands/hmset
-  return Keystore.command({
-		command = 'hmset',
-		key = 'state:' .. parm_name .. ':' .. parm_value,
-		args = states
-	})
+    -- http://redis.io/commands/hmset
+    return Keystore.command({
+        command = 'hmset',
+        key = 'state:' .. parm_name .. ':' .. parm_value,
+        args = states
+    })
 end
 
 -- remove all state specific lock or dwelling
 -- parm_name is 'lockID' or 'dwellingID', parm_value is the ID
 util.deleteState = function(parm_name, parm_value)
-	return Keystore.delete({
-		key = 'state:' .. parm_name .. ':' .. parm_value
-	})
+    return Keystore.delete({
+        key = 'state:' .. parm_name .. ':' .. parm_value
+    })
 end
+
+-- save lock state in timeseries for logging/reporting
+util.saveHistory = function(sn, attribute_name, value)
+    local metrics = {
+        device_sn = sn
+    }
+    -- metrics can't have dashes in the names
+    if attribute_name == 'lock-command' then
+        metrics.command = value
+    elseif attribute_name == 'lock-status' then
+        metrics.status = value
+    end
+    local out = Tsdb.write({
+        metrics = metrics
+    })
+end
+
 
 -- return a list of items of a particular parameter_name
 -- accessible to all of the user's roles. Returns a table like
 util.getUserAccessibleItems = function(user_id, parameter_name)
-	local roles = User.listUserRoles({id = user_id})
-	local items = {}
+    local roles = User.listUserRoles({ id = user_id })
+    local items = {}
 
-	function addItemsFromParameters(role)
-		for k, parameter in pairs(role.parameters) do
-			if parameter_name == nil or parameter.name == parameter_name then
-				local state = util.getStates(parameter.name, parameter.value)
-				local item = {
-					role_id = role.role_id,
-					[parameter.name] = parameter.value,
-					state = state
-				}	
-				table.insert(items, item)
-		  end	
-		end
-	end
+    function addItemsFromParameters(role)
+        for k, parameter in pairs(role.parameters) do
+            if parameter_name == nil or parameter.name == parameter_name then
+                local state = util.getStates(parameter.name, parameter.value)
+                local item = {
+                    role_id = role.role_id,
+                    [parameter.name] = parameter.value,
+                    state = state
+                }
+                table.insert(items, item)
+            end
+        end
+    end
 
-	for k, role in pairs(roles) do
-		addItemsFromParameters(role)
-	end
+    for k, role in pairs(roles) do
+        addItemsFromParameters(role)
+    end
 
-	return items
-end 
+    return items
+end
